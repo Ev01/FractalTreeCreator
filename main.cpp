1b45702e1a2b8e21e7d9c7cdfb88d60688cdc451
Ev01 <evpevdev@optusnet.com.au>
2025-11-03 23:20:07 +1100

    Middle click and drag to move. Scroll to zoom

--- main.cpp
+++ main.cpp
@@ -25,6 +25,9 @@ static TreeSpecies species = {3, 0.3, 50.0, {0, 0, 0}, 1.0, 1.0};
 static int depth = 4;
 static double sway = 0;
 
+static float treeX = 400;
+static float treeY = 20;
+
 
 static void rebuildTree(const TreeSpecies &species, float sway, int maxDepth)
 {
@@ -70,7 +73,9 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    ImGuiIO &io = ImGui::GetIO(); (void)io;
     ImGui_ImplSDL3_ProcessEvent(event);
+
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
     }
@@ -80,6 +85,33 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
         float newHeight = event->window.data2;
         Render::UpdateViewportSize(newWidth, newHeight);
     }
+
+    if (io.WantCaptureMouse) {
+        return SDL_APP_CONTINUE;
+    }
+
+    bool isMiddleMousePressed = SDL_GetMouseState(NULL, NULL) & SDL_BUTTON_MIDDLE;
+    if (isMiddleMousePressed && event->type == SDL_EVENT_MOUSE_MOTION) {
+        treeX += event->motion.xrel;
+        treeY -= event->motion.yrel;
+    }
+    else if (event->type == SDL_EVENT_MOUSE_WHEEL) {
+        int windowWidth, windowHeight;
+        SDL_GetWindowSize(Render::GetWindow(), &windowWidth, &windowHeight);
+        float zoomFactor = (event->wheel.y * 0.1 + 1.0);
+        // Difference between base of the tree and the mouse position
+        float xDiff = treeX - event->wheel.mouse_x;
+        float yDiff = treeY - ((float)windowHeight - event->wheel.mouse_y);
+        //SDL_Log("xDiff: %f, yDiff: %f", xDiff, yDiff);
+        species.baseBranchLen *= zoomFactor;
+        // Move the tree so it zooms in on the mouse, not the base of the tree.
+        treeX += xDiff * (zoomFactor-1.0);
+        treeY += yDiff * (zoomFactor-1.0);
+        rebuildTree(species, sway, depth);
+    }
+
+
+
     return SDL_APP_CONTINUE;
 }
 
@@ -109,18 +141,16 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
     int windowWidth, windowHeight;
     SDL_GetWindowSize(Render::GetWindow(), &windowWidth, &windowHeight);
-    static int treeX = windowWidth / 2;
-    static int treeY = windowHeight * 0.05;
 
     // UI Stuff
     ImGui::Begin("Tree Position");
-    ImGui::SliderInt("X", &treeX, 0, windowWidth);
-    ImGui::SliderInt("Y", &treeY, 0, windowHeight);
+    ImGui::SliderFloat("X", &treeX, 0.0f, (float)windowWidth);
+    ImGui::SliderFloat("Y", &treeY, 0.0f, (float)windowHeight);
     ImGui::End();
 
     ImGui::Render();
 
-    Render::DrawFrame(treeX, treeY);
+    Render::DrawFrame((int)treeX, (int)treeY);
 
     return SDL_APP_CONTINUE;
 }
